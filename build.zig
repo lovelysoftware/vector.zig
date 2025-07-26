e4b386b131da35493ca0037de6aeefe78389d1ed
Morgan Gallant <morgan@lovely.software>
2025-07-26 09:25:21 -0700

    initial project setup
    Signed-off-by: Morgan Gallant <morgan@lovely.software>

---
+++ build.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib_mod = b.createModule(.{
+        .root_source_file = b.path("src/vector.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "vector",
+        .root_module = lib_mod,
+    });
+    b.installArtifact(lib);
+
+    const test_filters = b.option(
+        []const []const u8,
+        "test-filters",
+        "Run specific unit tests",
+    ) orelse &[0][]const u8{};
+
+    const lib_unit_tests = b.addTest(.{
+        .root_module = lib_mod,
+        .filters = test_filters,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+}
